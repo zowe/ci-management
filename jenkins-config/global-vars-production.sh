531720e8e5d8d1c4702a5bf819b0912198175a43
Daniel Pono Takamori <dtakamori@contractor.linuxfoundation.org>
2019-06-18 10:55:47 -0700

    add environment variables and modify centos image name

---
+++ jenkins-config/global-vars-production.sh
@@ -0,0 +1,7 @@
+GIT_BASE=https://github.com/zowe/$PROJECT
+GIT_CLONE_URL=git@github.com:
+GIT_URL=https://github.com
+JENKINS_HOSTNAME=vex-sjc-zowe-jenkins-prod-1
+LOGS_SERVER=https://logs.zowe.org
+NEXUS_URL=https://nexus.zowe.org
+SILO=production
