7f479bcc232ed7d01d6e790b81070c300db791b4
Daniel Pono Takamori <dtakamori@contractor.linuxfoundation.org>
2019-06-19 09:04:06 -0700

    remove LOG_SERVER as it's optional

--- jenkins-config/global-vars-sandbox.sh
+++ jenkins-config/global-vars-sandbox.sh
@@ -3,6 +3,5 @@ GIT_CLONE_URL=git@github.com:
 GIT_URL=https://github.com
 OS_AUTH_URL=https://auth.vexxhost.net/v3
 JENKINS_HOSTNAME=vex-sjc-zowe-jenkins-prod-1
-LOGS_SERVER=https://logs.zowe.org
 NEXUS_URL=https://nexus.zowe.org
 SILO=sandbox
